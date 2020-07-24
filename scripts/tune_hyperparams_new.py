3890d121e4530159f8173da1f6cd5af7d554376d
Belinda Li <26785408+belindal@users.noreply.github.com>
2020-07-23 20:27:11 -0700

    tuning hyperparameters

--- scripts/tune_hyperparams_new.py
+++ scripts/tune_hyperparams_new.py
@@ -13,9 +13,11 @@ id2title = json.load(open("models/id2title.json"))
 
 all_save_dir = "/checkpoint/belindali/entity_link/saved_preds"
 data = "nq"
-split= "train2"
+split= "dev"
+model= 'wiki_all_ents;all_mention_biencoder_all_avg_true_128_true_true_bert_large_qa_linear;15'
+# model= 'finetuned_webqsp_all_ents;all_mention_biencoder_all_avg_true_20_true_true_bert_large_qa_linear'
 
-save_dir = "{}/{}_{}_finetuned_webqsp_all_ents;all_mention_biencoder_all_avg_true_20_true_true_bert_large_qa_linear_joint0.0_top50cands_final_joint_0/".format(all_save_dir, data, split)
+save_dir = "{}/{}_{}_{}_joint0.0_top50cands_final_joint_0/".format(all_save_dir, data, split, model)
 
 with open(os.path.join(save_dir, "biencoder_outs.jsonl")) as f:
     examples = f.readlines()
@@ -40,6 +42,9 @@ else:
     # inverse sigmoid
     mention_dists = [np.log(md / (1 - md)) for md in mention_dists]
 
+
+# threshold=-1.9
+threshold=-5
 new_examples = []
 num_correct=0
 num_predicted=0
@@ -52,16 +57,24 @@ for i, example in enumerate(tqdm(examples)):
     # (num_pred_mentions, 2)
     entity_mention_bounds_idx = pred_mention_bounds[i][cands_mask]
     mention_scores = mention_dists[i][cands_mask]
+    if len(mention_scores.shape) > 1:
+        mention_scores = mention_scores[:,0]
     # scores = 1/(1 + np.exp(-mention_dists[i][cands_mask])) + torch.log_softmax(torch.tensor(cand_dists[i][cands_mask]), 1).numpy()
     # take only the top cands
     # top_pred_entity = pred_entity_list[:,0]
     # top_entity_mention_bounds_idx = entity_mention_bounds_idx[:,0]
-    scores_mask = (mention_scores > -3) | (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log() > -5).numpy()
-    # scores_mask = (torch.sigmoid(torch.tensor(mention_scores)) > 0.25).numpy() & (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)) > 0).numpy()
+    
+    # scores_mask = (mention_scores > -3) | (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log() > -2).numpy()
+    scores_mask = (torch.log_softmax(torch.tensor(cand_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log() > threshold).numpy()  # GRAPHQUESTIONS BEST
+    # scores_mask = (mention_scores > -3) & (torch.log_softmax(torch.tensor(cand_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log() > -5).numpy()
+    # scores_mask = (mention_scores > -3) & (torch.log_softmax(torch.tensor(cand_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log() > -2).numpy()
+    # scores_mask = (mention_scores > -5) & ((mention_scores + cand_dists[i][cands_mask][:,0]) > 5)
+    # scores_mask = (torch.sigmoid(torch.tensor(mention_scores)) > 0.1).numpy() & (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)) > 0).numpy()
     # scores_mask = (mention_scores > -float("inf"))
     # sort...
-    _, sorted_idxs = (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log())[scores_mask].sort()
-    # _, sorted_idxs = (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)))[scores_mask].sort()
+    # _, sorted_idxs = (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log())[scores_mask].sort()
+    # _, sorted_idxs = (torch.log_softmax(torch.tensor(biencoder_dists[i][cands_mask]), 1)[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log())[scores_mask].sort()
+    _, sorted_idxs = ((torch.tensor(cand_dists[i][cands_mask]))[:,0] + torch.sigmoid(torch.tensor(mention_scores)).log())[scores_mask].sort(descending=True)
     # _, sorted_idxs = torch.tensor(mention_scores[scores_mask]).sort()
     threshold_entities = pred_entity_list[scores_mask][sorted_idxs]
     threshold_mention_bounds = entity_mention_bounds_idx[scores_mask][sorted_idxs]
@@ -89,6 +102,7 @@ for i, example in enumerate(tqdm(examples)):
     new_ex = {
         "id": example["id"],
         "text": example["text"],
+        "tokens": example["tokens"],
     }
     if "gold_triples" in example:
         all_pred_entities_pruned = []
@@ -96,9 +110,11 @@ for i, example in enumerate(tqdm(examples)):
         # ensure well-formed-ness, prune overlaps
         # greedily pick highest scoring, then prune all overlapping
         for idx, mb in enumerate(pred_triples):
+            if mb[1] >= len(mention_masked_utterance) or mb[2] >= len(mention_masked_utterance):
+                continue
             # check if in existing mentions
             try:
-                if mention_masked_utterance[mb[1]] == 1 or mention_masked_utterance[min(mb[2], len(mention_masked_utterance))-1] == 1:
+                if sum(mention_masked_utterance[mb[1]:mb[2]]) > 0:
                     continue
             except:
                 import pdb
@@ -107,7 +123,23 @@ for i, example in enumerate(tqdm(examples)):
             mention_masked_utterance[mb[1]:mb[2]] = 1
     else:
         all_pred_entities_pruned = pred_triples
-    # new_ex['pred_mentions'] = threshold_entities_translate
+        # all_pred_entities_pruned = []
+        # mention_masked_utterance = np.zeros(len(example['tokens']))
+        # ensure well-formed-ness, prune overlaps
+        # greedily pick highest scoring, then prune all overlapping
+        # for idx, mb in enumerate(pred_triples):
+        #     if mb[1] >= len(mention_masked_utterance) or mb[2] > len(mention_masked_utterance):
+        #         continue
+        #     # check if in existing mentions
+        #     try:
+        #         if sum(mention_masked_utterance[mb[1]:mb[2]]) > 0:
+        #             continue
+        #     except:
+        #         import pdb
+        #         pdb.set_trace()
+        #     all_pred_entities_pruned.append(mb)
+        #     mention_masked_utterance[mb[1]:mb[2]] = 1
+    new_ex['pred_mentions'] = threshold_entities_translate
     new_ex['pred_triples'] = all_pred_entities_pruned
     new_ex['pred_triples_string'] = [
         [id2title[triple[0]], tokenizer.decode(example['tokens'][triple[1]:triple[2]])]
@@ -130,7 +162,7 @@ f1 = 2*p*r / (p+r)
 print(f1)
 
 # save
-with open(os.path.join("/checkpoint/belindali/entity_link/data/{}/saved_preds".format(data), "{}.jsonl".format(split)), 'w') as wf:
+with open(os.path.join("/checkpoint/belindali/entity_link/data/{}/saved_preds".format(data), "{}_wiki_-5_no_overlaps.jsonl".format(split)), 'w') as wf:
     for new_ex in new_examples:
         b=wf.write(json.dumps(new_ex) + "\n")
 
